10a09dae2c77e9dfc1c48bc80c6c994a288e70ef
cbw <2250080455@qq.com>
2021-11-08 13:35:19 +0800

    配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,44 +3,69 @@ module.exports = {
     description: 'chenbwoen的学习笔记',
     // base: '/v1/adroi-h5/adroiapi/',
     themeConfig: {
-        logo:'/assets/img/logo.png',
+        logo: '/assets/img/logo.png',
         nav: [
             { text: '首页', link: '/' },
-            { text: '前端', link: '/frontEnd/' },
+            {
+                text: '前后端', items: [
+                    {
+                        text: '前端', link: '/frontEnd/', items: [
+                            { text: 'vue', link: '/frontEnd/vue/' },
+                            { text: 'react', link: '/frontEnd/react/' },
+                        ]
+                    },
+                    { text: '后端', link: '' },
+                ]
+            },
             { text: '大数据', link: '/bigData/' },
+            {
+                text: '第三方工具', items: [
+                    { text: 'GitHub', link: '/git/' },
+                    { text: 'markdown', link: '/markdown/' }
+                    // {
+                    //     text: 'markdown', items: [
+                    //         { text: 'markdown', link: '/markdown/' }
+                    //     ]
+                    // }
+                ]
+            }
         ],
-        sidebar:{
-            "/frontEnd/react/":[
+        sidebar: {
+            "/frontEnd/react/": [
                 "",
                 "react1"
             ],
-            "/frontEnd/vue/":[
+            "/frontEnd/vue/": [
                 "",
                 "vue1"
             ],
-            "/frontEnd/":[
-                "react/",
-                "vue/"
-            ],
-            "/bigData/JAVA/":[
+            // "/frontEnd/": [
+            //     {
+            //         title: '前端',
+            //         collapsable: false,
+            //         children: [
+            //             { title: 'react', path: '/frontEnd/react/' },
+            //             { title: 'vue', path: '/frontEnd/vue/' },
+            //         ]
+            //     }
+            // ],
+            "/bigData/JAVA/": [
                 ""
             ],
-            "/bigData/python/":[
+            "/bigData/python/": [
                 ""
             ],
-            "/bigData/":[
+            "/bigData/": [
                 "JAVA/",
                 "python/",
             ],
+            "/git/": [
+                ""
+            ],
+            "/markdown/": [
+                ""
+            ],
         },
-        // sidebar: {
-        //     '/': [
-        //         "/", //指的是根目录的md文件 也就是 README.md 里面的内容
-        //         "apiword", 根目录创建 apiword.md文件
-        //       "api", 根目录创建 api.md文件
-        //       "error" 根目录创建 error.md文件
-        //     ]
-        // },
         sidebarDepth: 2
     }
 }
